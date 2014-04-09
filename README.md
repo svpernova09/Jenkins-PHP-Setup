a3501272a5f539d34f6c2d81a9845e3a97e0aee1
Svpernova09 <joe@joeferguson.me>
2014-04-08 21:10:06 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Jenkins-PHP-Setup
+==
+
+These are my notes and other things related to playing with [Jenkins](http://www.jenkins-ci.org)
---
+++ install-php-tools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get install php5-cli curl php-pear
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+wget http://phpdox.de/releases/phpdox.phar
+chmod +x phpdox.phar
+sudo mv phpdox.phar /usr/local/bin/phpdox
+composer global require 'phpunit/phpunit=3.7.*'
+composer global require 'phploc/phploc=*'
+composer global require 'pdepend/pdepend=1.1.0'
+composer global require 'phpmd/phpmd=1.4.*'
+composer global require 'sebastian/phpcpd=*'
+pear install PHP_CodeSniffer
